8bdda2c1de6698278f5e1a7561fcfdcdfbb18c77
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-10-10 09:22:30 -0300

    update classes

--- src/br/com/dio/debbuging/Main.java
+++ src/br/com/dio/debbuging/Main.java
@@ -2,21 +2,28 @@ package br.com.dio.debbuging;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Início do método main.");
-        metodo1();
-        System.out.println("Fim do método main.");
+        System.out.println("Iniciou do programa no método main.");
+        a();
+        System.out.println("Finalizou do programa no método main.");
     }
 
-    private static void metodo1() {
-        System.out.println("Início do método1.");
-        metodo2();
-        System.out.println("Fim do método1.");
+    static void a() {
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a.");
     }
 
-    private static void metodo2() {
-        System.out.println("Início do médodo2.");
-        for(int i = 0; i <= 4; i++)
-            System.out.println(i);
-        System.out.println("Fim do método2");
+    static void b() {
+        System.out.println("Entrou no método b.");
+        for(int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
     }
+
+    static void c(){
+        System.out.println("Entrou no método c.");
+        //Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
+    }
+
 }
